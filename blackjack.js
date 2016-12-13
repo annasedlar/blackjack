d08156454da10c9301464c5fa8aa55432aac0f0f
Anna Sedlar <annasedlar@gmail.com>
2016-12-13 17:13:55 -0500

    first deal javascript

--- blackjack.js
+++ blackjack.js
@@ -1,5 +1,8 @@
 // GLOBALS
 var theDeck = [];
+createDeck();
+var playersHand = [];
+var dealersHand = [];
 
 
 
@@ -7,17 +10,28 @@ var theDeck = [];
 $(document).ready(function(){
 
 	$('.deal-button').click(function(){
-		console.log(this);
-		createDeck();
 		shuffleDeck();
+		//add card 0 to players hand (ie. the first card in our deck array which is shuffled
+		// ie - we dk what card it is)
+		playersHand.push(theDeck[0]);
+		dealersHand.push(theDeck[1]);
+		playersHand.push(theDeck[2]);
+		dealersHand.push(theDeck[3]);
+		
+		//put the first card in the players hand
+		placeCard(playersHand[0], "player", 'one');
+		placeCard(playersHand[1], "player", 'two');
+		placeCard(dealersHand[0], "dealer", 'one');
+		placeCard(dealersHand[1], "dealer", 'two');
+
+		calculateTotal('player', playersHand);
+		calculateTotal('dealer', dealersHand);
 	});
 
 	$('.hit-button').click(function(){
-		console.log(this);
 	});
 
 	$('.stand-button').click(function(){
-		console.log(this);
 	});
 });
 
@@ -41,10 +55,25 @@ function shuffleDeck(){
 	}
 }
 
+function placeCard(whatCard, who, whichSlot){
+	var classToTarget = '.'+who+'-cards .card-'+whichSlot;
+	console.log(classToTarget);
+	$(classToTarget).html('<img src="cards/' +whatCard+'.png">');
+};
 
 
+function calculateTotal(who, hand){
+	var cardValue = 0;
+	var total=0;
+	for(let i=0; i < hand.length; i++){
+		cardValue = Number(hand[i].slice(0, -1));
+		console.log(cardValue); 
+		total += cardValue
+	}
 
-
+	var classToTarget = '.'+who+'-total-number';
+	$(classToTarget).text(total);
+}
 
 
 
